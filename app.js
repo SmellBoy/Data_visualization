a39ee0a7d11cf1c9184a46f95338dc712e561f9b
jarnar <jarnar@qq.com>
2018-07-12 17:15:16 +0800

    update: add pm2

--- app.js
+++ app.js
@@ -86,4 +86,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
 module.exports = app;
